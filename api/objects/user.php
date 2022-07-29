57025d1fd26c92ad2207854ca10085530e08779d
Andrey Borodin <boroddon@gmail.com>
2022-07-29 14:35:45 +0300

    Update user.php

--- api/objects/user.php
+++ api/objects/user.php
@@ -4,34 +4,34 @@ class User{
 	private $table_name = "api_users";
 
 	public $id;
-    public $login;
-    public $password;
-    
-    public function __construct($db){
-        $this->conn = $db;
-    }
+	public $login;
+	public $password;
+	
+	public function __construct($db){
+		$this->conn = $db;
+	}
 
-    public function auth(){
+	public function auth(){
 
-	    $query = 'SELECT
-	                u.id, u.login, u.password
-	            FROM
-	                '. $this->table_name .' u
-	             WHERE 
-	             	u.login = ?
-             	LIMIT
-                	0,1';
+		$query = 'SELECT
+					u.id, u.login, u.password
+				FROM
+					'. $this->table_name .' u
+				 WHERE 
+					u.login = ?
+				LIMIT
+					0,1';
 
-	    $stmt = $this->conn->prepare($query);
-	    $stmt->bindParam(1, $this->login);
-    	$stmt->execute();
-	    
-    	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam(1, $this->login);
+		$stmt->execute();
+		
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    	if($row['id']){
-    		if (password_verify($this->password, $row["password"])) {
+		if($row['id']){
+			if (password_verify($this->password, $row["password"])) {
 				return $row;
 			}
-    	}
+		}
 	}
 }
