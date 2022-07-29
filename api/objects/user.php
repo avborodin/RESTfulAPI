66d5ca5717d06da1ddcb03b6bcdb1dc058443f88
Andrey Borodin <boroddon@gmail.com>
2022-07-29 14:37:13 +0300

    Update user.php

--- api/objects/user.php
+++ api/objects/user.php
@@ -1,37 +1,37 @@
 <?php
 class User{
-	private $conn;
-	private $table_name = "api_users";
+    private $conn;
+    private $table_name = "api_users";
 
-	public $id;
-	public $login;
-	public $password;
-	
-	public function __construct($db){
-		$this->conn = $db;
-	}
+    public $id;
+    public $login;
+    public $password;
+    
+    public function __construct($db){
+        $this->conn = $db;
+    }
 
-	public function auth(){
+    public function auth(){
 
-		$query = 'SELECT
-					u.id, u.login, u.password
-				FROM
-					'. $this->table_name .' u
-				 WHERE 
-					u.login = ?
-				LIMIT
-					0,1';
+        $query = 'SELECT
+                    u.id, u.login, u.password
+                FROM
+                    '. $this->table_name .' u
+                 WHERE 
+                    u.login = ?
+                LIMIT
+                    0,1';
 
-		$stmt = $this->conn->prepare($query);
-		$stmt->bindParam(1, $this->login);
-		$stmt->execute();
-		
-		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->login);
+        $stmt->execute();
+        
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-		if($row['id']){
-			if (password_verify($this->password, $row["password"])) {
-				return $row;
-			}
-		}
-	}
+        if($row['id']){
+            if (password_verify($this->password, $row["password"])) {
+                return $row;
+            }
+        }
+    }
 }
